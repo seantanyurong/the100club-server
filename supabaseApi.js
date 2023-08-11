a21db9d0144b8d93bf981d86ba153d6b74f73bbd
Sean Tan <seantan1218@gmail.com>
2023-08-11 17:09:29 +0800

    Added supabase service key

--- supabaseApi.js
+++ supabaseApi.js
@@ -2,5 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.REACT_APP_SUPABASE_SERVICE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseServiceKey);
