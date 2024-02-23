89799a8e2f1d95c5b80fbae6eb5ccec12b78bcf7
joesurf <desictiverk@gmail.com>
2024-02-23 10:16:06 +0800

    Update config

--- helper/notification.js
+++ helper/notification.js
@@ -1,13 +1,10 @@
 import axios from 'axios'
 
 export const send_notification_telegram = async (message) => {
-    const supergroup_id = "-1002040302052"
-    const hokages_supergroup_id = "-1002045069509"
-
     await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
-        chat_id: hokages_supergroup_id,
+        chat_id: process.env.TELEGRAM_GROUP_ID,
         text: message,
-        message_thread_id: '796',
+        message_thread_id: process.env.MESSAGE_THREAD_ID,
     })
     .then(function (response) {
         console.log(response);
