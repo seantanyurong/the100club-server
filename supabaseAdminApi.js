c0b813aa1eee67ee913f9451f9949e61565d3af2
Sean Tan <seantanyurong@gmail.com>
2024-10-02 20:50:48 +0800

    feature: add helper script

--- supabaseAdminApi.js
+++ supabaseAdminApi.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseServiceKey = process.env.REACT_APP_SUPABASE_SERVICE_KEY;
 
-export default supabase = createClient(supabaseUrl, supabaseServiceKey, {
+export default createClient(supabaseUrl, supabaseServiceKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
--- supabaseApi.js
+++ supabaseApi.js
@@ -1,7 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import 'dotenv/config.js';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 const supabaseServiceKey = process.env.REACT_APP_SUPABASE_SERVICE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey);
